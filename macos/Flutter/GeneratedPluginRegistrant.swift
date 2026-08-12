c5bef29fa50e4d0b476799176ed3b7515a9a7f35
kaysha <kayshahiciano@gmail.com>
2026-08-12 12:34:13 -0400

    fix: conflictos 2

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_darwin
-<<<<<<< HEAD
 import sqflite_darwin
-=======
->>>>>>> origin/feature/about
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-<<<<<<< HEAD
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-=======
->>>>>>> origin/feature/about
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
